90a7e183064a740a38f0586a6a8ff668460ce5ea
Pierrot Lamontagne <pierrot.lamontagne@umontreal.ca>
2023-02-04 15:39:22 -0500

    ajout des données de pollution de l'air

--- Codes/reading_air_pollution.py
+++ Codes/reading_air_pollution.py
@@ -1,10 +1,50 @@
 import pandas as pd
+import numpy as np
+from grid import x, y
 
-import sys
+def find_nearest(array, value):
+    
+    """
+    Trouve l'indice du point le plus près d'une valeur donnée dans un array donné
+
+    ### Paramètres
+    array: Le array dans lequel chercher l'indice
+    value: La valeur pour laquelle on veut trouver le point le plus proche
+    
+    ### Retourne
+    idx: L'indice du point le plus près de la valeur donnée
+    
+    """
+    
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    
+    return idx
 
 
 # Reading the data
 
-data = pd.read_csv('../Data/air_pollution/rsqa-indice-qualite-air-station.csv')
+data = pd.read_csv("../Data/air_pollution/rsqa-indice-qualite-air-station.csv",delimiter=",")
 
-print(data)
+stationId = data["stationId"]
+longitude = data["longitude"]
+latitude = data["latitude"]
+polluant = data["polluant"]
+quantite = data["valeur"] #De 0 à 500
+
+air_pollution_dict = dict()
+air_pollution_dict["i_Xgrid"] = np.zeros(len(stationId))
+air_pollution_dict["i_Ygrid"] = np.zeros(len(stationId))
+air_pollution_dict["valeur"] = np.zeros(len(stationId))
+
+for i in range(len(stationId)): 
+
+    #On trouve l'élément de la grid le plus proche de l'emplacement du stationId
+    i_Xgrid = find_nearest(latitude[i],x)
+    i_Ygrid = find_nearest(longitude[i],y)
+    
+    air_pollution_dict["i_Xgrid"][i] = i_Xgrid
+    air_pollution_dict["i_Ygrid"][i] = i_Ygrid
+    
+    #On trouve la quantité de polluant
+    air_pollution_dict["valeur"][i] = quantite[i]
