b59da4302b58ac7086a24c344036fbc16e37b94e
Pierrot Lamontagne <pierrot.lamontagne@umontreal.ca>
2023-02-04 16:20:01 -0500

    began the master file

---
+++ Codes/point_object.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from reading_air_pollution import air_pollution_dict
+
+class point: 
+    def __init__(self,x,y,t):
+        
+        #Coordonnées de la cas
+        self.x = x 
+        self.y = y
+        
+        #Temps
+        self.t = t
+        
+        #fill temperature
+
+        
+        #fill pollution air
+        self.pollution_air = 
+        
+        #fill pluie
+        
+        #fill_neige
+        
+        #fill_parc
+        
+        #fill_acoustique
+        
+        
+        
+        #Facteurs environnementaux
+        self.temperature = temperature
+        self.pollution_air = pollution_air
+        self.pluie = pluie
+        self.neige = neige
+        self.parc = parc
+        self.acoustique = acoustique
+        
+
+def fill_temperature(t): 
+    
+    return
+
+def fill_pollution_air(x,y): 
+    
+    return
+
+def fill_pluie(t): 
+    
+    return
+
+def fill_neige(t): 
+    
+    return
+
+def fill_parc(x,y): 
+    
+    return
+
+def fill_acoustique(x,y): 
+    
+    return
+
+
+        
+    
+        
