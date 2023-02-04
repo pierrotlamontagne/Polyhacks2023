d1e13783a53902ee4df26b795a1c85bdef18a97c
Felix St-Denis <felix.st-denis@umontreal.ca>
2023-02-04 14:22:03 -0500

    la grille tbk

--- Codes/grid.py
+++ Codes/grid.py
@@ -4,10 +4,15 @@ import numpy as np
 Montréal Dimensions : 
     boomerang -> 52 km par 18 km
     rectangle -> 52 km par 18 km  
+
+    J'ai pris 2 points à chaque extrémité de l'île. 
 """
+latlong_longueur1 = [45.417665, -73.96712]
+latlong_longueur2 = [45.702069, -73.477873]
 
-x = np.arange(0, 52*1000, 25)
-y = np.arange(0, 18*1000, 25)
 
-X_grid, Y_grid = np.meshgrid(x, y)
+x = np.linspace(latlong_longueur1[0], latlong_longueur2[0], 10000)
+y = np.linspace(latlong_longueur1[1], latlong_longueur2[1], 10000)
+
+[X_grid, Y_grid] = np.meshgrid(x, y)
 
