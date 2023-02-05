cb32dee3716b23cd45da1973de458a228d5796ba
Zachary Mann <zachary.et.mann@gmail.com>
2023-02-04 23:05:38 -0500

    m

--- Codes/generate_grid.py
+++ Codes/generate_grid.py
@@ -2,6 +2,27 @@ import numpy as np
 import random as rand
 import json
 
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+mtl_poly = Polygon(
+    [
+    (45.407230, -73.957201),
+    (45.437012, -73.969255),
+    (45.510384, -73.849193),
+    (45.546528, -73.692003),
+    (45.628188, -73.621605),
+    (45.680091, -73.531438),
+    (45.689186, -73.483702),
+    (45.551593, -73.529027),
+    (45.470159, -73.551689),
+    (45.416031, -73.614855),
+    (45.437688, -73.756615),
+    (45.419754, -73.876677),
+    (45.406891, -73.919109),
+    ]
+)
+
 latlong_longueur1 = [45.417665, -73.96712]
 latlong_longueur2 = [45.702069, -73.477873]
 
@@ -17,18 +38,23 @@ dict = {
 
 for i in range(len(x)-1):
     for j in range(len(y)-1):
-        feat_dict = {
-            "type":"Feature",
-            "geometry":{
-                "type":"Polygon",
-                "coordinates":[[[y[j],x[i]],[y[j+1],x[i]],[y[j+1],x[i+1]],[y[j],x[i+1]],[y[j],x[i]]]],
-            },
-            "properties":{
-                "probPerSector":rand.randint(1,7),
-            },
-        }
 
-        dict["features"].append(feat_dict)
+        point = Point(x[i],y[j])
+
+        if mtl_poly.contains(point):
+
+            feat_dict = {
+                "type":"Feature",
+                "geometry":{
+                    "type":"Polygon",
+                    "coordinates":[[[y[j],x[i]],[y[j+1],x[i]],[y[j+1],x[i+1]],[y[j],x[i+1]],[y[j],x[i]]]],
+                },
+                "properties":{
+                    "probPerSector":rand.randint(1,7),
+                },
+            }
+
+            dict["features"].append(feat_dict)
 
 json_object = json.dumps(dict)
 
